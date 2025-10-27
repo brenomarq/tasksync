38843ca64a6118ae126907d5978833e1bdae7122
Breno Marques <brenomarques516@gmail.com>
2025-10-27 11:47:59 -0300

    :tada: init: first commit

---
+++ src/main/java/com/breno/tasksync/TasksyncApplication.java
@@ -0,0 +1,13 @@
+package com.breno.tasksync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breno/tasksync/TasksyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breno.tasksync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
